bd8d38473c05b4e09cef7874fc483c9565ad9618
torsten.thiele <torsten.thiele@immoware24.de>
2025-03-26 15:44:51 +0100

    modify dockerfile

--- vevor-ble-bridge/run.sh
+++ vevor-ble-bridge/run.sh
@@ -5,23 +5,18 @@ bashio::log.info "Starte Bluetooth-Dienst..."
 bluetoothctl power on || bashio::log.warning "Konnte Bluetooth nicht einschalten"
 
 # Konfiguration aus den Add-on-Optionen auslesen
-#MQTT_HOST=$(bashio::config 'mqtt_host')
-#MQTT_PORT=$(bashio::config 'mqtt_port')
-#MQTT_USERNAME=$(bashio::config 'mqtt_username')
-#MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-#MQTT_TOPIC=$(bashio::config 'mqtt_topic')
-#SCAN_INTERVAL=$(bashio::config 'scan_interval')
-
-# Konfiguration aus options.json lesen
-export MQTT_HOST=$(jq --raw-output '.mqtt_host' /data/options.json)
-export MQTT_PORT=$(jq --raw-output '.mqtt_port' /data/options.json)
-export MQTT_USERNAME=$(jq --raw-output '.mqtt_username' /data/options.json)
-export MQTT_PASSWORD=$(jq --raw-output '.mqtt_password' /data/options.json)
+MQTT_HOST=$(bashio::config 'mqtt_host')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USERNAME=$(bashio::config 'mqtt_username')
+MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+MQTT_TOPIC=$(bashio::config 'mqtt_topic')
+SCAN_INTERVAL=$(bashio::config 'scan_interval')
 
 
+bashio::log.info "Config fertig gelesen"
 
 # Starte die Bridge
 cd /app
-bashio::log.info "Starte Vevor BLE Bridge..."
+bashio::log.info "Starte Vevor BLE Bridge... logging to mqtt server ${mqtt_host}"
 # Starte das Python-Skript
 python3 /app/main.py
